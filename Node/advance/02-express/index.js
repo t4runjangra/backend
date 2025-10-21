954f604344547964ef2e7040461c9aa94a68c2ae
tarun <tarunjangra1903@gmail.com>
2025-10-21 18:33:01 +0530

    logger middleware utils

--- Node/advance/02-express/index.js
+++ Node/advance/02-express/index.js
@@ -1,6 +1,32 @@
 import express from "express";
+import "dotenv/config";
+import logger from "./logger.js";
+import morgan from "morgan";
+ 
+
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
+
+const morganFormat = ":method :url :status :response-time ms";
+
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 
 
 // a simple crud app using express 
