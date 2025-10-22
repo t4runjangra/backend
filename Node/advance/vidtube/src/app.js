df645fc4d300629c17f6dbd602eceab7d79f6029
tarun <tarunjangra1903@gmail.com>
2025-10-22 20:04:29 +0530

    mongoose bcrypt

--- Node/advance/vidtube/src/app.js
+++ Node/advance/vidtube/src/app.js
@@ -11,10 +11,19 @@ app.use(
 )
 
 app.use(express.json({limit:"16kb"}))
-
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-
 app.use(express.static("public"))
+
+
+// import routes 
+import healtcheckRouter from './routes/healthcheck-route.js'
+
+
+
+//routes
+
+
+app.use("/api/v1/healthcheck",healtcheckRouter)
 
 
 
--- Node/advance/vidtube/src/routes/healthcheck-route.js
+++ Node/advance/vidtube/src/routes/healthcheck-route.js
@@ -1,8 +1,11 @@
 import Router from "express";
-import { healthCheck } from "../controller/healthcheck-controllers.js";
+import { healthCheck } from "../controllers/healthcheck-controller.js";
 
 const router = Router()
 
+
+//   /api/v1/healthcheck
 router.route("/").get(healthCheck)
+router.route("/test").get(healthCheck)
 
 export default router
