3fb0e87fd425089c66c86071a632e66100e7570d
tarun <tarunjangra1903@gmail.com>
2025-10-22 12:28:55 +0530

    utilities and health check are done

---
+++ Node/advance/vidtube/src/db/db-index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URL}/${DB_NAME}`,
+            { useNewUrlParser: true, useUnifiedTopology: true }
+        );
+        console.log(
+            `\nMongoDB is Connected! DB host: ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.log("MongoDB Connection Error ", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- Node/advance/vidtube/src/index.js
+++ Node/advance/vidtube/src/index.js
@@ -1,1 +1,20 @@
-console.log("hello gaiz");
+import dotenv from 'dotenv'
+import { app } from "./app.js";
+import connectDB from './db/db-index.js';
+
+
+dotenv.config({
+    path: "./.env"
+})
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running`);
+        })
+    })
+    .catch((err) => {
+        console.log("Mongodb connection error", err)
+    })
