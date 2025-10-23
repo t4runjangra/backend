62ab0882878ad1694eb2027aaadf8181af7da6de
tarun <tarunjangra1903@gmail.com>
2025-10-23 23:29:16 +0530

    multer and cloudinary

--- Node/advance/vidtube/src/app.js
+++ Node/advance/vidtube/src/app.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser";
+
 
 const app = express()
 
@@ -10,10 +12,13 @@ app.use(
     })
 )
 
+// middlewares
+
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-
+app.use(cookieParser())
 
 // import routes 
 import healtcheckRouter from './routes/healthcheck-route.js'
