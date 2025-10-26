3358b4c2d4b38fbc7ea3ebaf10e87f5b183eff66
tarun <tarunjangra1903@gmail.com>
2025-10-27 01:19:16 +0530

    login functionality added

--- Node/advance/vidtube/src/controllers/user-controller.js
+++ Node/advance/vidtube/src/controllers/user-controller.js
@@ -4,6 +4,25 @@ import { apiResponse } from "../utils/api-response.js"
 import { User } from "../models/user.models.js"
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.utils.js"
 
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        if (!user) {
+            console.log("User Not found")
+            process.exit(1)
+        }
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
+    } catch (error) {
+        throw new apiError(500, "Something went wrong while generating the access Token and refresh Token ", error);
+
+    }
+}
+
+
 const registerUser = asyncHandler(async (req, res) => {
 
     console.log('BODY:', req.body);
@@ -25,7 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
     if (existedUser) {
         console.log("here");
         console.error("user Exist")
-        throw new apiError(404 , "User with email or usename already exists")
+        throw new apiError(404, "User with email or usename already exists")
     }
 
     const avatarLocalPath = req.files?.avatar?.[0]?.path;
@@ -100,11 +119,50 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 const login = asyncHandler(async (req, res) => {
+
+    const { email, password } = req.body
+
+    if (!email) {
+        throw new apiError(404, "Email is required");
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new apiError(404,"User Not found");
+        
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(password)
+    if (!isPasswordCorrect) {
+        throw new apiError(401," Invalid Creds");
+    }
     
-    
-    
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    if (!loggedInUser) {
+        throw new apiError(404, "Error in log in ", error);
+    }
+
+    const options = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "development"
+    }
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new apiResponse(
+        200,
+        {user: loggedInUser,accessToken, refreshToken},
+        "User Logged in successfully"))
 })
 
+
+
+
 export {
-    registerUser
+    registerUser,
+    login
 }
