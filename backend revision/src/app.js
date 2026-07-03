a7751224637eedd325ba1e6e969e4476bdbdbe18
t4runjangra <tarunjangra1903@gmail.com>
2026-07-03 23:51:46 +0530

    Add CORS support to the application

--- backend revision/src/app.js
+++ backend revision/src/app.js
@@ -3,12 +3,22 @@ import { userRoute } from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import noteRouter from "./routes/note.route.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 
 const app = express();
 
+
 app.use(express.json())
+
+
 app.use(cookieParser());
+
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials:true
+}))
+
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
